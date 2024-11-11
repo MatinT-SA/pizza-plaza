c88c828449562f06e4ce04343fc940903e00c3d4
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-11-11 20:22:43 +0330

    adding pizza component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello React</h1>;
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/salamino.jpg" alt="Salamino Pizza" />
+      <h2>Pizza Salamino</h2>
+      <p>Tomato, mozarella, and pepperoni</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
