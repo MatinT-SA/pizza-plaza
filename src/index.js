45a71a6fa32e21deabeb2ceb76821b62bba00008
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-11-15 12:41:30 +0330

    extracting order JSX into a new and separate component

--- src/index.js
+++ src/index.js
@@ -112,18 +112,24 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>
-            We're open until {closeHour}:00. Please visit us or order online.
-          </p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We'll be at your service between {openHour} and {closeHour}
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Please visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
